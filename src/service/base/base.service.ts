7a3532d52400e7c9d67653e8533d1302f95b5b13
Komang Suryadana <komang.suryadana@frisidea.com>
2020-11-24 22:07:44 +0700

    Prod environment

--- src/service/base/base.service.ts
+++ src/service/base/base.service.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api/v1/';
+const BASE_URL = 'http://suyuncdn-backend.farmasiexperience.com/api/v1/';
 
 const instance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
